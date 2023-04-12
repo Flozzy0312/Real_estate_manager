867a599f387670d367be53bc3f2f7c34680f6997
Florian <florian.lautescher@gmail.com>
2023-04-12 15:34:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/realestatemanager/ItemDetailHostActivity.java
@@ -0,0 +1,66 @@
+package com.example.realestatemanager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.realestatemanager.databinding.ActivityItemDetailBinding;
+
+public class ItemDetailHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityItemDetailBinding binding = ActivityItemDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_item_detail);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.
+                Builder(navController.getGraph())
+                .build();
+    getSupportActionBar();
+    //TODO
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                // Do something for menu item 1
+                return true;
+            case R.id.action_add:
+                //TODO 
+                // Do something for menu item 2
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_item_detail);
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+}
